7038d6da7da0e6dadd5615e4f4d08ed5fc6b9a31
HepinSuthar <sutharhepin2610@gmail.com>
2026-04-24 14:07:30 +0530

    fix port production

--- backend/src/server.js
+++ backend/src/server.js
@@ -57,5 +57,5 @@ io.on("connection", (socket) => {
     // handle disconnect if needed
   });
 });
-
-server.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
